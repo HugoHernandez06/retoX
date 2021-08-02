e5c14b2f777b55141df728aba2d1a0fa0189abeb
Hugo Hernandez Cardenas <hugoa.hernandez@upb.edu.co>
2021-08-02 11:04:27 -0700

    point 2 part 2 in progress

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 
 void funcPrint(void);
+void funcAl1(void);
+
 
 int main(void) {
     
     funcPrint();
+    funcAl1();
     printf("Hola Mundo\n");
     return EXIT_SUCCESS;
 
@@ -13,4 +16,9 @@ int main(void) {
 
 void funcPrint(void){
     printf("Hola Mundo\n");
+}
+
+void funcAl1(void){
+    printf("Hi\n");
+    printf("Bye\n");
 }
