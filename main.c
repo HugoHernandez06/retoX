a21cfbc9d1b91d9366676ffb9ef8a7b7b8d388e0
Hugo Hernandez Cardenas <hugoa.hernandez@upb.edu.co>
2021-08-02 10:34:52 -0700

    add funcPrint

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void funcPrint(void);
+
 int main(void) {
     
-
+    funcPrint();
     printf("Hola Mundo\n");
     return EXIT_SUCCESS;
 
+}
+
+void funcPrint(void){
+    printf("Hola Mundo\n");
 }
